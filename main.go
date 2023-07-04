df11481dc6b446eeddfda34919466c8825f07a00
mbpswangxg <xg.wang@mbpsmartec.co.jp>
2023-07-04 16:31:56 +0900

    update docker-compose.yml

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	// "fmt"
+	// "os"
 	"github.com/lqwangxg/yseed/common"
 	"net/http"
 	"github.com/labstack/echo"
@@ -10,25 +10,20 @@ import (
 )
 
 func main() {
-	//fmt.Printf("os.Args=%v, idx=%s \n", os.Args, idx)   
-	// if g, err := common.GetGuaById(idx); err == nil {                                    
-    //     fmt.Printf("id=%d, name=%s\n", g.Id, g.Name)                              
-    // } else{
-	// 	fmt.Printf("os.Args=%v, err: %v\n", os.Args, err)   
-	// }
 	//yseed()
 	openServer()
 }
-func yseed(){
-	lenArgs :=len(os.Args)
-	fmt.Printf("os.Args Length = %d, 3 integer numbers are required.\n", lenArgs) 
-	if(lenArgs<3){
-		return;
-	}
-	// to the console.
-	msg := common.Seed(os.Args[1],os.Args[2],os.Args[3])
-	fmt.Println(msg)
-}
+
+// func yseed(){
+// 	lenArgs :=len(os.Args)
+// 	fmt.Printf("os.Args Length = %d, 3 integer numbers are required.\n", lenArgs) 
+// 	if(lenArgs<3){
+// 		return;
+// 	}
+// 	// to the console.
+// 	msg := common.Seed(os.Args[1],os.Args[2],os.Args[3])
+// 	fmt.Println(msg)
+// }
 func openServer(){
 	e := echo.New()
     //===============================================
@@ -44,9 +39,6 @@ func openServer(){
 		return c.String(http.StatusOK,"Hello, get the prediction by /predict?n3=(n1,n2,n3)")
     })
     e.GET("/predict", common.Show)
-    // e.GET("/users/:id", getUser)
-    // e.PUT("/users/:id", updateUser)
-    // e.DELETE("/users/:id", deleteUser)
 
     e.Logger.Fatal(e.Start(":8000"))
 }
